66640c72c25d75fa472bf2531922069d2ac34c50
SpeedOfSmell <vldmirea@gmail.com>
2019-01-11 12:27:46 -0500

    Highlight element when user mouses over

--- elementPicker.js
+++ elementPicker.js
@@ -1,9 +1,22 @@
 $("body").wrap("<div class='grey_out'></div>");
 
+var oldColor;
+var oldElem = null;
+var currentElem = null;
 document.addEventListener("mousemove", function(e) {
 	var x = e.clientX;
 	var y = e.clientY;
 	
-	//var element = document.elementFromPoint;
-	console.log(x + " " + y);
+	currentElem = document.elementFromPoint(x, y);
+	if (currentElem != oldElem) {
+		if (oldElem) {
+			oldElem.style.backgroundColor = oldColor;
+		}
+		
+		oldColor = currentElem.style.backgroundColor;
+		oldElem = currentElem;
+		
+		currentElem.style.backgroundColor = "#FDFF47";
+	}
+	
 });
