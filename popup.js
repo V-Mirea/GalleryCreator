fec74644b475d4f551e50268c6cabf99eac436c4
SpeedOfSmell <vldmirea@gmail.com>
2019-01-12 01:40:35 -0500

    Add persistent flags

--- popup.js
+++ popup.js
@@ -1,18 +1,37 @@
-var elementPickerInjected = false;
-var elementPickerRunning = false;
-var button = document.getElementById("pickElement");
+var elementPickerInjected;
+var elementPickerRunning;
 
-button.onclick = function() {
-	if (!elementPickerInjected) {
-		chrome.tabs.insertCSS(null, {file: 'greyOut.css'}, function() {
-			chrome.tabs.executeScript(null, {file: 'jquery-3.3.1.min.js'}, function() {
-				chrome.tabs.executeScript(null, {file: 'elementPicker.js'}, toggleElementPicker);
-			});
-		});
+chrome.storage.local.get("elementPickerInjected", function(data) {
+	if (typeof data.elementPickerInjected === "undefined") {
+		chrome.storage.local.set({elementPickerInjected: false});
+		elementPickerInjected = false;
 	} else {
-		toggleElementPicker();
+		elementPickerInjected = data.elementPickerInjected;
 	}
-}
+
+	chrome.storage.local.get("elementPickerRunning", function(data) {
+		if (typeof data.elementPickerRunning === "undefined") {
+			chrome.storage.local.set({elementPickerRunning: false});
+			elementPickerRunning = false;
+		} else {
+			elementPickerRunning = data.elementPickerRunning;
+		}
+
+		var button = document.getElementById("pickElement");
+
+		button.onclick = function() {
+			if (!elementPickerInjected) {
+				chrome.tabs.insertCSS(null, {file: 'greyOut.css'}, function() {
+					chrome.tabs.executeScript(null, {file: 'jquery-3.3.1.min.js'}, function() {
+						chrome.tabs.executeScript(null, {file: 'elementPicker.js'}, toggleElementPicker);
+					});
+				});
+			} else {
+				toggleElementPicker();
+			}
+		}
+	});
+});
 
 function toggleElementPicker() {
 	var message = elementPickerRunning ? "stopElementPicker" : "runElementPicker";
@@ -20,6 +39,7 @@ function toggleElementPicker() {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.sendMessage(tabs[0].id, message, function(response) {
 			elementPickerRunning = response.running;
+			chrome.storage.local.set({elementPickerRunning: elementPickerRunning});
 		});
 	});
 }
