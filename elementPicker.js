ba2c3814035ae042391c549281756ca040ae5f6c
SpeedOfSmell <vldmirea@gmail.com>
2019-01-12 00:54:32 -0500

    Add logic to toggle elementPicker

--- elementPicker.js
+++ elementPicker.js
@@ -1,11 +1,36 @@
 $("body").wrap("<div class='grey_out'></div>");
 
+screenDimmed = false;
+
 var oldColor;
 var oldElem = null;
 var currentElem = null;
-document.addEventListener("mousemove", function(e) {
-	var x = e.clientX;
-	var y = e.clientY;
+
+chrome.runtime.onMessage.addListener(
+	function (message, sender, sendResponse) {
+		if (message == "runElementPicker") {
+			runElementPicker();
+			sendResponse({running: true});
+		} else if (message == "stopElementPicker") {
+			stopElementPicker();
+			sendResponse({running: false});
+		}
+	}
+);
+
+function runElementPicker() {
+	dimScreen();
+	document.addEventListener("mousemove", highlightElement);
+}
+
+function stopElementPicker() {
+	undimScreen();
+	document.removeEventListener("mousemove", highlightElement);
+}
+
+function highlightElement(event) {
+	var x = event.clientX;
+	var y = event.clientY;
 	
 	currentElem = document.elementFromPoint(x, y);
 	if (currentElem != oldElem) {
@@ -18,5 +43,24 @@ document.addEventListener("mousemove", function(e) {
 		
 		currentElem.style.backgroundColor = "#FDFF47";
 	}
+}
+
+function dimScreen() {
+	$(".grey_out").css("display", "block");
+
+	screenDimmed = true;
+}
+
+function undimScreen() {
+	$(".grey_out").css("display", "none");
 	
-});
+	screenDimmed = false;
+}
+
+function toggleScreenDimness() {
+	if (screenDimmed) {
+		undimScreen();
+	} else {
+		dimScreen();
+	}
+}
--- popup.js
+++ popup.js
@@ -1,5 +1,25 @@
+var elementPickerInjected = false;
+var elementPickerRunning = false;
 var button = document.getElementById("pickElement");
 
 button.onclick = function() {
-	chrome.runtime.sendMessage("runElementPicker");
+	if (!elementPickerInjected) {
+		chrome.tabs.insertCSS(null, {file: 'greyOut.css'}, function() {
+			chrome.tabs.executeScript(null, {file: 'jquery-3.3.1.min.js'}, function() {
+				chrome.tabs.executeScript(null, {file: 'elementPicker.js'}, toggleElementPicker);
+			});
+		});
+	} else {
+		toggleElementPicker();
+	}
+}
+
+function toggleElementPicker() {
+	var message = elementPickerRunning ? "stopElementPicker" : "runElementPicker";
+
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, message, function(response) {
+			elementPickerRunning = response.running;
+		});
+	});
 }
