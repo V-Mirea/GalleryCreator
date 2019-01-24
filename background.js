fe21dc1d12c8f6584d0fffb1ce267949c67480cb
SpeedOfSmell <vldmirea@gmail.com>
2019-01-24 01:11:57 -0500

    Add check to send images to gallery script

--- background.js
+++ background.js
@@ -1,7 +1,12 @@
+var images = null;
+
 chrome.runtime.onMessage.addListener(
     function(request, sender, sendResponse) {
         if (request.action == "openPage") {
             chrome.tabs.create({url: request.page});
+            images = request.images;
+        } else if (request == "getImages") {
+            sendResponse({images: images});
         }
     }
 );
