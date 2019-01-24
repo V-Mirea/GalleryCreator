47f89e5a98ced51d2e00a5385980c78db33f9512
SpeedOfSmell <vldmirea@gmail.com>
2019-01-23 22:35:19 -0500

    Communicate with content script to check if running instead of storing a flag

--- popup.js
+++ popup.js
@@ -1,26 +1,12 @@
-var elementPickerInjected;
 var elementPickerRunning;
 
-chrome.storage.local.get("elementPickerInjected", function(data) {
-	if (typeof data.elementPickerInjected === "undefined") {
-		chrome.storage.local.set({elementPickerInjected: false});
-		elementPickerInjected = false;
-	} else {
-		elementPickerInjected = data.elementPickerInjected;
-	}
+var button = document.getElementById("pickElement");
 
-	chrome.storage.local.get("elementPickerRunning", function(data) {
-		if (typeof data.elementPickerRunning === "undefined") {
-			chrome.storage.local.set({elementPickerRunning: false});
-			elementPickerRunning = false;
-		} else {
-			elementPickerRunning = data.elementPickerRunning;
-		}
-
-		var button = document.getElementById("pickElement");
-
-		button.onclick = function() {
-			if (!elementPickerInjected) {
+button.onclick = function() {
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+		chrome.tabs.sendMessage(tabs[0].id, "injected?", function(response) {
+			response = response || {};
+			if (!response.injected) {
 				chrome.tabs.insertCSS(null, {file: 'greyOut.css'}, function() {
 					chrome.tabs.executeScript(null, {file: 'jquery-3.3.1.min.js'}, function() {
 						chrome.tabs.executeScript(null, {file: 'elementPicker.js'}, toggleElementPicker);
@@ -29,17 +15,12 @@ chrome.storage.local.get("elementPickerInjected", function(data) {
 			} else {
 				toggleElementPicker();
 			}
-		}
+		});
 	});
-});
+}
 
 function toggleElementPicker() {
-	var message = elementPickerRunning ? "stopElementPicker" : "runElementPicker";
-
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.sendMessage(tabs[0].id, message, function(response) {
-			elementPickerRunning = response.running;
-			chrome.storage.local.set({elementPickerRunning: elementPickerRunning});
-		});
+		chrome.tabs.sendMessage(tabs[0].id, "toggleElementPicker", {});
 	});
 }
