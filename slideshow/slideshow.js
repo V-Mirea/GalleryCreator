4f17689efbc32e012f1cd68a6072edf5f08ecef2
SpeedOfSmell <vldmirea@gmail.com>
2019-01-28 00:03:48 -0500

    Add functioning image menu to navbox

--- slideshow/slideshow.js
+++ slideshow/slideshow.js
@@ -1,27 +1,50 @@
 var mImages = null;
 var mTitle = null;
-var index = 0;
+var mGalleryIndex = 0;
 
 $(document).ready(function() {
+    console.log("Started");
     chrome.runtime.sendMessage("getImages", function(response) {
         mImages = response.images;
         mTitle = response.title || "Slideshow";
 
         $(".title").text(mTitle);
-        replaceImgElement(mImages[index]);
+        for(var i = 0; i < mImages.length; i++) {
+            var elem = `
+                <li class="img-link">
+                </li>
+            `;
+            elem = $(elem).text(i+1);
+            $(".nav-body .links").append(elem);
+        }
+
+        replaceImgElement(0);
+
+        var links = document.getElementsByClassName("img-link");
+        for (var i = 0; i < links.length; i++) {
+            links[i].addEventListener('click', function(event) {
+                replaceImgElement($(event.target).text() - 1);
+            });
+        }
     });
 });
 
-function replaceImgElement(url) {
+function replaceImgElement(index) {
     $(".display").remove();
-    galleryElement = "<img class=\"display\" src=\"" + url + "\"></img>"
+    galleryElement = "<img class=\"display\" src=\"" + mImages[index] + "\"></img>"
     $(".gallery").append(galleryElement);
+
+    $(".image-count").text(index + 1 + "/" + mImages.length);
+
+    document.getElementsByClassName("img-link")[mGalleryIndex].style.backgroundColor = null;
+    mGalleryIndex = index;
+    document.getElementsByClassName("img-link")[index].style.backgroundColor = "grey";
 }
 
 document.onkeydown = function(event) {
-    if (event.keyCode == '37' && index > 0) { //left
-        replaceImgElement(mImages[--index]);
-    } else if (event.keyCode == '39' && index < mImages.length - 1) { //right
-        replaceImgElement(mImages[++index]);
+    if (event.keyCode == '37' && mGalleryIndex > 0) { //left
+        replaceImgElement(mGalleryIndex - 1);
+    } else if (event.keyCode == '39' && mGalleryIndex < mImages.length - 1) { //right
+        replaceImgElement(mGalleryIndex + 1);
     }
 }
