b1a8bd63be7d4bdd74772b39c238f211ed863e0f
SpeedOfSmell <vldmirea@gmail.com>
2019-01-28 16:23:49 -0500

    Add delete item function

--- gallery/gallery.js
+++ gallery/gallery.js
@@ -1,14 +1,15 @@
-var images = null;
+var mImages = null;
+var mContextMenuElement = null;
 
 $(document).ready(function() {
     chrome.runtime.sendMessage("getImages", function(response) {
-        images = response.images;
+        mImages = response.images;
 
-        for(let i = 0; i < images.length; i++) {
+        for(let i = 0; i < mImages.length; i++) {
             var element = `
                 <div class="image">
-                    <a target="_blank" href="` + images[i] + `">
-                        <img src="` + images[i] + `" alt="Image missing">
+                    <a target="_blank" href="` + mImages[i] + `">
+                        <img src="` + mImages[i] + `" alt="Image missing">
                     </a>
                 </div>
             `;
@@ -16,8 +17,30 @@ $(document).ready(function() {
         }
 
         $(".footer").click(function() {
-            message = {action: "openPage", page: chrome.extension.getURL("slideshow/slideshow.html"), images: images};
+            message = {action: "openPage", page: chrome.extension.getURL("slideshow/slideshow.html"), images: mImages};
 		    chrome.runtime.sendMessage(message);
         });
     });
 });
+
+document.addEventListener("mousedown", function(event) {
+    if(event.button == 2) { // right click
+        mContextMenuElement = $(event.target); // Keep track of last element user right-clicked 
+    }
+}, true);
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+    if (info.menuItemId == "deleteImage") {
+        deleteImage(mContextMenuElement);
+    }
+});
+
+function deleteImage(element) {
+    //console.log($("img[src='" + url + "']"));
+
+    var url = mContextMenuElement.attr('src');
+    mContextMenuElement.closest(".image").css("display", "none");
+
+    var index = mImages.indexOf(url);
+    if (index !== -1) mImages.splice(index, 1);
+}
